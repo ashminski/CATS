8346cac3c968b0571f1a71e303dcefd8a12b4c6d
Ashley Kaminski <ashleymk@vt.edu>
2012-11-28 20:46:29 -0500

    Added part of onclicklistener

--- CATS_app/src/edu/vt/ece4564/cats_app/MainActivity.java
+++ CATS_app/src/edu/vt/ece4564/cats_app/MainActivity.java
@@ -3,10 +3,12 @@ package edu.vt.ece4564.cats_app;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	
 	private EditText usernameField;
 	private EditText passwordField;
@@ -23,6 +25,8 @@ public class MainActivity extends Activity {
         loginButton = (Button) findViewById(R.id.loginButton);
         createAccountButton = (Button) findViewById(R.id.newAccountButton);
         
+        loginButton.setOnClickListener(this);
+        createAccountButton.setOnClickListener(this);
     }
 
     @Override
@@ -30,4 +34,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View arg0) {
+		if(arg0.getId() == R.id.loginButton){
+			//validate login information, if good, then pass to next activity
+			//else, pop up dialog
+		}
+		else if(arg0.getId() == R.id.newAccountButton){
+			//prompt for phone number, make sure valid number
+			//save new account, go to next activity
+		}
+	}
 }
